321c1642849ce108f527a7278443b6393b4e744e
Isabela Moraes <isabelagmoraes@hotmail.com>
2021-12-04 10:24:37 -0300

    Desafio: Refactoring de classes e TypeScript

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import { FiPlusSquare } from "react-icons/fi";
 
 import { Container } from "./styles";
@@ -8,28 +7,22 @@ interface Props {
   openModal: () => void;
 }
 
-class Header extends Component<Props> {
-  render() {
-    const { openModal } = this.props;
-
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button type="button" onClick={openModal}>
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    );
-  }
+export default function Header({ openModal }: Props) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button type="button" onClick={openModal}>
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  );
 }
-
-export default Header;
